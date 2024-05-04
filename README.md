e01c921ea29c38d65fd431bb2aa8f84556157b0d
janderson.santos <janderson.santos@sydle.com>
2024-05-04 18:54:31 -0300

    feat: server mult-thread

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Trabalho-Redes-de-Computadores-TCP
-Este trabalho prático proporcionará aos alunos uma experiência prática no desenvolvimento de sistemas distribuídos simples, além de oferecer insights sobre os desafios e considerações envolvidos na criação de aplicativos cliente-servidor. 
+
+Este trabalho prático proporcionará aos alunos uma experiência prática no desenvolvimento de sistemas distribuídos simples, além de oferecer insights sobre os desafios e considerações envolvidos na criação de aplicativos cliente-servidor.
 
 **Linguagem:** C
 
 **Tipo de conexão:** TCP
 
 ## Descrição Sobre a atividade:
+
 Neste trabalho prático, os alunos devem desenvolver uma aplicação console
 simulando o funcionamento básico do aplicativo Uber, com um cliente fazendo o
 papel do usuário e um servidor fazendo o papel do motorista. O objetivo é
 proporcionar aos alunos uma compreensão prática de como funciona a
 comunicação entre clientes e servidores com conexão TCP.
+
+##Execução do Programa
+**Build and Run: É necessário apenas rodar um comando "Make" no diretorio do projeto
+**Executando servidor: ./server
+\*\*Executando client: ./client
--- server.c
+++ server.c
@@ -1,5 +1,6 @@
 #include "common.h"
 
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +16,37 @@ void usage(int argc, char **argv)
     printf("usage: %s <v4|v6> <server port>\n", argv[0]);
     printf("example: %s v4 51511", argv[0]);
     exit(EXIT_FAILURE);
+}
+
+struct client_data
+{
+    int csock;
+    struct sockaddr_storage storage;
+};
+
+void *client_thread(void *data)
+{
+
+    struct client_data *cdata = (struct client_data *)data;
+    struct sockaddr *caddr = (struct sockaddr *)(&cdata->storage);
+
+    char caddrstr[BUFSZ];
+    addrtostr(caddr, caddrstr, BUFSZ);
+    printf("[log] connection from %s\n", caddrstr);
+
+    char buf[BUFSZ];
+    memset(buf, 0, BUFSZ);
+    size_t count = recv(cdata->csock, buf, BUFSZ - 1, 0);
+    printf("[msg] %s, %d bytes %s\n", caddrstr, (int)count, buf);
+
+    sprintf(buf, "remote endpoint: %.1000s\n", caddrstr);
+    count = send(cdata->csock, buf, strlen(buf) + 1, 0);
+    if (count != strlen(buf) + 1)
+    {
+        logexit("send");
+    }
+    close(cdata->csock);
+    pthread_exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **argv)
@@ -39,6 +71,12 @@ int main(int argc, char **argv)
         logexit("socket");
     }
 
+    int enable = 1;
+    if (0 != setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)))
+    {
+        logexit("setsockopt");
+    }
+
     struct sockaddr *addr = (struct sockaddr *)(&storage);
     if (0 != bind(s, addr, sizeof(storage)))
     {
@@ -59,28 +97,23 @@ int main(int argc, char **argv)
         struct sockaddr_storage cstorage;
         struct sockaddr *caddr = (struct sockaddr *)(&cstorage);
         socklen_t caddrlen = sizeof(cstorage);
+
         int csock = accept(s, caddr, &caddrlen); // socket que conversa com o cliente
         if (csock == -1)
         {
             logexit("accept");
         }
 
-        char caddrstr[BUFSZ];
-        addrtostr(caddr, caddrstr, BUFSZ);
-        printf("[log] connection from %s\n", caddrstr);
-
-        char buf[BUFSZ];
-        memset(buf, 0, BUFSZ);
-        size_t count = recv(csock, buf, BUFSZ, 0);
-        printf("[msg] %s, %d bytes %s\n", caddrstr, (int)count, buf);
-
-        sprintf(buf, "remote endpoint: %.1000s\n", caddrstr);
-        count = send(csock, buf, strlen(buf) + 1, 0);
-        if (count != strlen(buf) + 1)
+        struct client_data *cdata = malloc(sizeof(*cdata));
+        if (!cdata)
         {
-            logexit("send");
+            logexit("malloc");
         }
-        close(csock);
+        cdata->csock = csock;
+        memcpy(&(cdata->storage), &cstorage, sizeof(cstorage));
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, client_thread, cdata);
     }
 
     exit(EXIT_SUCCESS);
